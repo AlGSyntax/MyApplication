805d8401879aa846c56412408b8fd0a6046063e3
alvaro <adelcastifo@gmail.com>
2023-11-24 15:35:52 +0100

    Die Aufgabe wurde gelöst.

--- app/src/main/java/Itemadapter/FlashcardAdapter.kt
+++ app/src/main/java/Itemadapter/FlashcardAdapter.kt
@@ -8,17 +8,36 @@ import com.example.myapplication.databinding.ListItemBinding
 import model.Flashcard
 
 class FlashcardAdapter(
-    open val flashcards: List<Flashcard>
+    val flashcards: MutableList<Flashcard>,
+    private val onEdit: (Flashcard, Int) -> Unit,
+    private val onDelete: (Int) -> Unit
 ) : RecyclerView.Adapter<FlashcardAdapter.FlashcardViewHolder>() {
 
     // ViewBinding für die list_item.xml
     inner class FlashcardViewHolder(val binding: ListItemBinding) : RecyclerView.ViewHolder(binding.root) {
+        init {
+            binding.editIcon.setOnClickListener {
+                val position = adapterPosition
+                if (position != RecyclerView.NO_POSITION) {
+                    onEdit(flashcards[position], position)
+                }
+            }
+
+            // Setze den OnClickListener für das Löschen-Icon
+            binding.deleteIcon.setOnClickListener {
+                val position = adapterPosition
+                if (position != RecyclerView.NO_POSITION) {
+                    onDelete(position)
+                }
+            }
+        }
+
         fun bind(flashcard: Flashcard) {
             binding.textViewQuestion.text = flashcard.question
             binding.textViewAnswer.text = flashcard.answer
 
             // OnClickListener für die Karte, um die Antwort ein-/auszublenden
-            itemView.setOnClickListener {
+            binding.materialCardView.setOnClickListener {
                 if (binding.textViewAnswer.visibility == View.GONE) {
                     binding.textViewAnswer.visibility = View.VISIBLE
                 } else {
@@ -29,7 +48,6 @@ class FlashcardAdapter(
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FlashcardViewHolder {
-        // Erstellen des ViewBindings für die list_item.xml
         val layoutInflater = LayoutInflater.from(parent.context)
         val binding = ListItemBinding.inflate(layoutInflater, parent, false)
         return FlashcardViewHolder(binding)
--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -12,101 +12,92 @@ import com.example.myapplication.databinding.ActivityMainBinding
 import data.LerncartaDataSource
 import model.Flashcard
 
-
-
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var adapter: FlashcardAdapter
+    private val flashcards = LerncartaDataSource().flashcards.toMutableList()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val flashcards = LerncartaDataSource().flashcards
+        setupRecyclerView()
+        setupButtonListeners()
+    }
 
-        adapter = FlashcardAdapter(flashcards)
+    private fun setupRecyclerView() {
+        adapter = FlashcardAdapter(flashcards,
+            onEdit = { flashcard, position ->
+                editFlashcard(flashcard, position)
+            },
+            onDelete = { position ->
+                flashcards.removeAt(position)
+                adapter.notifyItemRemoved(position)
+            }
+        )
         binding.recyclerView.layoutManager = LinearLayoutManager(this)
         binding.recyclerView.adapter = adapter
+    }
 
+    private fun setupButtonListeners() {
         binding.addButton.setOnClickListener {
-            val addDialogView = LayoutInflater.from(this).inflate(R.layout.add_flashcard_dialog, null)
-            val dialog = AlertDialog.Builder(this)
-                .setTitle("Neue Flashcard hinzufügen")
-                .setView(addDialogView)
-                .setPositiveButton("Hinzufügen", null)
-                .setNegativeButton("Abbrechen", null)
-                .show()
+            addNewFlashcard()
+        }
+    }
 
-            dialog.getButton(AlertDialog.BUTTON_POSITIVE).setOnClickListener {
-                // Hier greifst du auf die Textfelder des Dialogs zu
-                val questionInput = addDialogView.findViewById<EditText>(R.id.questionInput)
-                val answerInput = addDialogView.findViewById<EditText>(R.id.answerInput)
-                val question = questionInput.text.toString().trim()
-                val answer = answerInput.text.toString().trim()
+    private fun addNewFlashcard() {
+        val addDialogView = LayoutInflater.from(this).inflate(R.layout.add_flashcard_dialog, null)
+        val dialog = AlertDialog.Builder(this)
+            .setTitle("Neue Flashcard hinzufügen")
+            .setView(addDialogView)
+            .setPositiveButton("Hinzufügen", null)
+            .setNegativeButton("Abbrechen", null)
+            .show()
 
-                // Prüfe, ob die Eingaben gültig sind
-                if (question.isNotEmpty() && answer.isNotEmpty()) {
-                    val newFlashcard = Flashcard(question, answer)
-                    (adapter.flashcards as MutableList).add(newFlashcard)
-                    adapter.notifyItemInserted(adapter.flashcards.size - 1)
-                    dialog.dismiss()
-                } else {
-                    Toast.makeText(this, "Frage und Antwort dürfen nicht leer sein.", Toast.LENGTH_SHORT).show()
-                }
+        dialog.getButton(AlertDialog.BUTTON_POSITIVE).setOnClickListener {
+            val questionInput = addDialogView.findViewById<EditText>(R.id.questionInput)
+            val answerInput = addDialogView.findViewById<EditText>(R.id.answerInput)
+            val question = questionInput.text.toString().trim()
+            val answer = answerInput.text.toString().trim()
+
+            if (question.isNotEmpty() && answer.isNotEmpty()) {
+                val newFlashcard = Flashcard(question, answer)
+                flashcards.add(newFlashcard)
+                adapter.notifyItemInserted(flashcards.size - 1)
+                dialog.dismiss()
+            } else {
+                Toast.makeText(this, "Frage und Antwort dürfen nicht leer sein.", Toast.LENGTH_SHORT).show()
             }
         }
-        binding.editButton.setOnClickListener {
-            if (adapter.flashcards.isNotEmpty()) {
-                // Wähle hier, welche Flashcard bearbeitet werden soll
-                val positionToEdit = 0 // Dies solltest du entsprechend anpassen
-                val flashcardToEdit = adapter.flashcards[positionToEdit]
+    }
 
-                val editDialogView = LayoutInflater.from(this).inflate(R.layout.edit_flashcard_dialog, null)
-                val questionInput = editDialogView.findViewById<EditText>(R.id.questionInput).apply {
-                    setText(flashcardToEdit.question)
-                }
-                val answerInput = editDialogView.findViewById<EditText>(R.id.answerInput).apply {
-                    setText(flashcardToEdit.answer)
-                }
+    private fun editFlashcard(flashcard: Flashcard, position: Int) {
+        val editDialogView = LayoutInflater.from(this).inflate(R.layout.edit_flashcard_dialog, null)
+        val questionInput = editDialogView.findViewById<EditText>(R.id.questionInput)
+        val answerInput = editDialogView.findViewById<EditText>(R.id.answerInput)
+        questionInput.setText(flashcard.question)
+        answerInput.setText(flashcard.answer)
 
-                val dialog = AlertDialog.Builder(this)
-                    .setTitle("Flashcard bearbeiten")
-                    .setView(editDialogView)
-                    .setPositiveButton("Aktualisieren", null)
-                    .setNegativeButton("Abbrechen", null)
-                    .show()
+        val dialog = AlertDialog.Builder(this)
+            .setTitle("Flashcard bearbeiten")
+            .setView(editDialogView)
+            .setPositiveButton("Aktualisieren", null)
+            .setNegativeButton("Abbrechen", null)
+            .show()
 
-                dialog.getButton(AlertDialog.BUTTON_POSITIVE).setOnClickListener {
-                    val updatedQuestion = questionInput.text.toString().trim()
-                    val updatedAnswer = answerInput.text.toString().trim()
+        dialog.getButton(AlertDialog.BUTTON_POSITIVE).setOnClickListener {
+            val updatedQuestion = questionInput.text.toString().trim()
+            val updatedAnswer = answerInput.text.toString().trim()
 
-                    if (updatedQuestion.isNotEmpty() && updatedAnswer.isNotEmpty()) {
-                        val updatedFlashcard = Flashcard(updatedQuestion, updatedAnswer)
-                        (adapter.flashcards as MutableList)[positionToEdit] = updatedFlashcard
-                        adapter.notifyItemChanged(positionToEdit)
-                        dialog.dismiss()
-                    } else {
-                        Toast.makeText(this, "Frage und Antwort dürfen nicht leer sein.", Toast.LENGTH_SHORT).show()
-                    }
-                }
+            if (updatedQuestion.isNotEmpty() && updatedAnswer.isNotEmpty()) {
+                val updatedFlashcard = Flashcard(updatedQuestion, updatedAnswer)
+                flashcards[position] = updatedFlashcard
+                adapter.notifyItemChanged(position)
+                dialog.dismiss()
+            } else {
+                Toast.makeText(this, "Frage und Antwort dürfen nicht leer sein.", Toast.LENGTH_SHORT).show()
             }
         }
-        binding.deleteButton.setOnClickListener {
-            if (adapter.flashcards.isNotEmpty()) {
-                // Wähle hier, welche Flashcard gelöscht werden soll
-                val positionToDelete = 0 // Dies solltest du entsprechend anpassen
-                AlertDialog.Builder(this)
-                    .setTitle("Flashcard löschen")
-                    .setMessage("Bist du sicher, dass du diese Flashcard löschen möchtest?")
-                    .setPositiveButton("Löschen") { dialog, which ->
-                        (adapter.flashcards as MutableList).removeAt(positionToDelete)
-                        adapter.notifyItemRemoved(positionToDelete)
-                    }
-                    .setNegativeButton("Abbrechen", null)
-                    .show()
-            }
-        }
-
     }
 }
